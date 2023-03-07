80f96daa8534dd12322f75443d7bf5a520d3154d
IvarMateo80 <Ivar.Olarte@fundacion-jala.org>
2023-03-07 18:31:01 -0400

    actu, cactus largo
    cactus largo añadido, entregado un poco tarde

---
+++ dino_runner/components/obstacles/cactus.py
@@ -0,0 +1,14 @@
+import random
+from dino_runner.components.obstacles.obstacle import Obstacle
+
+class CactusSmall (Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0,2)
+        super().__init__(image, self.type)
+        self.rect.y = 325
+
+class CactusLarge (Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0,2)
+        super().__init__(image, self.type)
+        self.rect.y = 310
---
+++ dino_runner/components/obstacles/obstaclemanager.py
@@ -0,0 +1,30 @@
+import random
+import pygame
+from dino_runner.components.obstacles.cactus import CactusLarge, CactusSmall
+from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
+
+class ObstacleManager:
+    def __init__(self):
+        self.obstacles = []
+
+    def update(self, game):
+        if len(self.obstacles) == 0:
+            self.type = random.randint(0,1)
+            if self.type == 0:
+                self.obstacles.append(CactusSmall(SMALL_CACTUS))
+            elif self.type == 1:
+                self.obstacles.append(CactusLarge(LARGE_CACTUS))
+            
+        for obstacle in self.obstacles:
+            obstacle.update(game.game_speed, self.obstacles)
+            if game.player.dino_rect.colliderect(obstacle.rect):
+                pygame.time.delay(1000)
+                game.playing = False
+                break
+
+    def draw(self, screen):
+        for obstacle in self.obstacles:
+            obstacle.draw(screen)
+
+    
+        
